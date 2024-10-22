631dff81b9028ddd2603ca8d40448acd278d2ad5
M L <martinn.lindbladd@gmail.com>
2024-10-22 13:26:01 +0200

    builder, pyinstaller

--- builder.bat
+++ builder.bat
@@ -1,1 +1,1 @@
-py -m PyInstaller --onefile main.py -n "Launcher" --collect-submodules "main.py" --collect-submodules "game.py" --collect-submodules "networkingagent.py"
+venv\Scripts\python.exe -m PyInstaller main.py -n "Game" --onefile --collect-submodules "networkingagent.py" --collect-submodules "game.py" --collect-submodules "gameobject.py"  --collect-submodules "scenario.py"
