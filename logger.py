6d2728169c83eeb58f785cd24fe9583c4417a0ac
M L <martinn.lindbladd@gmail.com>
2024-10-23 15:48:53 +0200

    logger data

--- logger.py
+++ logger.py
@@ -6,11 +6,16 @@ class Logger:
     def __init__(self) -> None:
         if (not os.path.isfile(f"{DATAPATH}{Logger.DATAFILE}")):
             open(Logger.DATAFILE, "x")
+    
+        savefile = open(f"{DATAPATH}{Logger.DATAFILE}", "w")
+        savefile.write(f"")
+        savefile.close()
+    
 
 
     def Log(self, data):
         savefile = open(f"{DATAPATH}{Logger.DATAFILE}", "a")
-        savefile.write(data)
+        savefile.write(f"{data}\n")
         savefile.close()
 
 logger = Logger()
