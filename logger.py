a361a579d3ede8dd7b6329cd8fc2e8cfb696a045
M L <martinn.lindbladd@gmail.com>
2024-10-23 14:01:25 +0200

    logger

---
+++ logger.py
@@ -0,0 +1,16 @@
+import os
+#from main import DATAPATH
+DATAPATH = "data/"
+class Logger:
+    DATAFILE = "debug.logger"
+    def __init__(self) -> None:
+        if (not os.path.isfile(f"{DATAPATH}{Logger.DATAFILE}")):
+            open(Logger.DATAFILE, "x")
+
+
+    def Log(self, data):
+        savefile = open(f"{DATAPATH}{Logger.DATAFILE}", "w")
+        savefile.write(data)
+        savefile.close()
+
+logger = Logger()
